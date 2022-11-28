b156c4c08d01e12687ceb724c82d667fef9da06c
Mascariep <edipohc@gmail.com>
2022-11-28 16:23:43 -0300

    Exercícios aula

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "../components/Header";
 import styled from "styled-components";
+import axios from "axios";
 
 const Form = styled.form`
   display: flex;
@@ -9,17 +10,26 @@ const Form = styled.form`
   gap: 5px;
 `;
 function HomePage() {
-  const [form, setForm] = useState({ email: "", senha: "" });
+  const [form, setForm] = useState({ email: "", password: "" });
 
   const onChange = (event) => {
     const { name, value } = event.target;
     setForm({ ...form, [name]: value });
   };
 
+  //login: edipo@gmail.com
+  //senha: teste
   const submitForm = (event) => {
     event.preventDefault();
+    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labeX/edipo/login', form).then((response)=>{
+    localStorage.setItem("token", response.data.token)
+    })
+    .catch(err =>{
+    console.log(err)
+    })
     console.log(form);
   };
+
   return (
     <main>
       <Header />
@@ -27,23 +37,25 @@ function HomePage() {
       <Form onSubmit={submitForm}>
         <label htmlFor="email">Login</label>
         <input
+          autoComplete="false"
           id="email"
           name="email"
           type="text"
-          input="form.email"
+          value={form.email}
           onChange={onChange}
           placeholder="email"
           required
           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
         />
-        <label htmlFor="senha">Senha</label>
+        <label htmlFor="password">Senha</label>
         <input
-          id="senha"
-          name="senha"
+          autoComplete="false"
+          id="password"
+          name="password"
           type="password"
-          input="form.senha"
+          value={form.password}
           onChange={onChange}
-          placeholder="senha"
+          placeholder="password"
           required
         />
         <button>Login</button>
